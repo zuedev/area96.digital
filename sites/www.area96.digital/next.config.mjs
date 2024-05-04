c8b250e61df932bf78f361c58d36dcedd6f988f9
zuedev <zuedev@gmail.com>
2024-05-04 16:05:18 +0100

    add legal docs redirects

--- sites/www.area96.digital/next.config.mjs
+++ sites/www.area96.digital/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/privacy-policy",
+        destination:
+          "https://github.com/zuedev/area96.digital/blob/main/documentation/privacy-policy.md",
+        permanent: true,
+      },
+      {
+        source: "/terms-of-service",
+        destination:
+          "https://github.com/zuedev/area96.digital/blob/main/documentation/terms-of-service.md",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
